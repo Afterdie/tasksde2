03f22448e1de6fd43b00fe086663c73e8a30d8fe
Akshat <82810411+Afterdie@users.noreply.github.com>
2024-08-12 23:40:31 +0530

    added headers

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,16 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
-
+//new additions
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://your-client-domain.com"); // Replace with your client's domain
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
